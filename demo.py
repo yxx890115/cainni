a5883cc7428cda3a82706bfb64ab6904d00ad3ec
yx <ajbf101058@qq.com>
2019-07-24 23:37:02 +0800

    新建demo

---
+++ demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/24 23:35
+# @Author  : YX
+# @Site    : 
+# @File    : demo.py
+# @Software: PyCharm
+def fun():
+    print('ufn')
